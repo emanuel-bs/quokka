7f84ee295cfe72bc945b4f34dec6b3826a2dfd05
Emanuel <emanuel-bs@users.noreply.github.com>
2021-02-17 23:42:27 -0300

    ENV variable QUOKKA_SERVER to point main server

--- run-workers.sh
+++ run-workers.sh
@@ -1,4 +1,7 @@
 cd ~/quokka
-sudo python3 workers/quokka_worker.py -H ${1:-15} -W capture -Q localhost -S 111-111-111 -C rabbitmq &
-sudo python3 workers/quokka_worker.py -H ${1:-15} -W portscan -Q localhost -S 111-111-111 -C rabbitmq &
-sudo python3 workers/quokka_worker.py -H ${1:-15} -W traceroute -Q localhost -S 111-111-111 -C rabbitmq &
+# Before execution the enviroment variable QUOKKA_SERVER must be set to: 
+#       <localhost> when running all together 
+#       <IP|FQDN> remote instalation of core functions
+sudo python3 workers/quokka_worker.py -H ${1:-15} -W capture -Q ${QUOKKA_SERVER} -S 111-111-111 -C rabbitmq &
+sudo python3 workers/quokka_worker.py -H ${1:-15} -W portscan -Q ${QUOKKA_SERVER} -S 111-111-111 -C rabbitmq &
+sudo python3 workers/quokka_worker.py -H ${1:-15} -W traceroute -Q ${QUOKKA_SERVER} -S 111-111-111 -C rabbitmq &
